ce38113e230d6254f97ce37d19611e0de96c90c2
unknown <32sinjinihomroy@gmail.com>
2023-08-02 23:02:28 +0530

    first

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "67406c5a1996d05c2132679fa3931ac4",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
